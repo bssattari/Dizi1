91e5cfa531117e0033ebafd082891904254775e3
unknown <b.s.sattari@gmail.com>
2023-09-03 14:28:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dizi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] sayilar=new int[10]{25,42,89,6,3,6,9,78,15,34};
+            int[] sayilar = new int[10];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                sayilar[i] = rnd.Next(1,100);
+                Console
+                    .WriteLine("{0}. Indexteki Sayı = {1}",
+                    i,
+                    sayilar[i].ToString());
+            }
+        }
+    }
+}
